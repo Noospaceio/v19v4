7f9bec6d5f8635b3f8a2346fd0e318f5302d983a
Noospaceio <helferhotline@proton.me>
2025-09-09 21:44:33 +0000

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,25 +1,25 @@
-import '../styles/global.css'
-import React from 'react'
-import { ConnectionProvider, WalletProvider } from '@solana/wallet-adapter-react';
+import '../styles/global.css';
+import { WalletAdapterNetwork } from '@solana/wallet-adapter-base';
+import {
+    ConnectionProvider,
+    WalletProvider,
+} from '@solana/wallet-adapter-react';
 import { WalletModalProvider } from '@solana/wallet-adapter-react-ui';
-import { PhantomWalletAdapter } from '@solana/wallet-adapter-phantom';
+import { PhantomWalletAdapter } from '@solana/wallet-adapter-wallets';
 import { clusterApiUrl } from '@solana/web3.js';
 
-require('@solana/wallet-adapter-react-ui/styles.css');
+export default function MyApp({ Component, pageProps }) {
+    const network = WalletAdapterNetwork.Devnet;
+    const endpoint = clusterApiUrl(network);
+    const wallets = [new PhantomWalletAdapter()];
 
-export default function App({ Component, pageProps }) {
-  // Use devnet by default; override via NEXT_PUBLIC_SOLANA_NETWORK
-  const network = process.env.NEXT_PUBLIC_SOLANA_NETWORK || 'devnet';
-  const endpoint = process.env.NEXT_PUBLIC_SOLANA_RPC_URL || clusterApiUrl(network);
-  const wallets = [ new PhantomWalletAdapter() ];
-
-  return (
-    <ConnectionProvider endpoint={endpoint}>
-      <WalletProvider wallets={wallets} autoConnect>
-        <WalletModalProvider>
-          <Component {...pageProps} />
-        </WalletModalProvider>
-      </WalletProvider>
-    </ConnectionProvider>
-  )
+    return (
+        <ConnectionProvider endpoint={endpoint}>
+            <WalletProvider wallets={wallets} autoConnect>
+                <WalletModalProvider>
+                    <Component {...pageProps} />
+                </WalletModalProvider>
+            </WalletProvider>
+        </ConnectionProvider>
+    );
 }
